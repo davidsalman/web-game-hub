f0d8d180139ad60741e63488850626b8eaa60c1f
David Salman <d.salman777@gmail.com>
2026-03-21 19:50:45 -0400

    feat: install and configure opennext/cloudflare adapter for cloudflare deployment

--- next.config.ts
+++ next.config.ts
@@ -52,3 +52,8 @@ const nextConfig: NextConfig = {
 }
 
 export default nextConfig
+
+// Enable calling `getCloudflareContext()` in `next dev`.
+// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
+// import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
+// initOpenNextCloudflareForDev()
